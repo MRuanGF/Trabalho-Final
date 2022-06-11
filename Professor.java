a9492e1a7e293b489e495f4e63249c44ec07a1be
CaioEstatistico <107193552+CaioEstatistico@users.noreply.github.com>
2022-06-11 14:32:39 -0300

    Add files via upload
    Adicionei a classe professor

---
+++ Professor.java
@@ -0,0 +1,23 @@
+class Professor{
+  private String siape;
+  private String unidadeLotacao;
+
+  public String getSiape(){
+    return siape;
+  }
+
+  public String getUnidade(){
+    return unidadeLotacao;
+  }
+
+  public void setSiape(String s){
+    siape = s;
+  }
+
+  public void setUnidade(String u){
+    unidadeLotacao = u;
+  }
+
+
+  
+}
